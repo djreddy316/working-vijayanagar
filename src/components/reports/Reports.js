cfefb0f7635dfeb2bb40ff123b80eca04f29d283
djreddy316 <djreddy316@gmail.com>
2020-09-05 05:10:55 +0530

    commitest1

--- src/components/reports/Reports.js
+++ src/components/reports/Reports.js
@@ -1,5 +1,8 @@
 import React,{useState,useEffect} from "react";
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
+
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
