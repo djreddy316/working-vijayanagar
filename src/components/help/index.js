b4b6bf2625b6c5d4aaa8f4108a83eb3e0234d79e
Janakiram <janakiram.reddy@intersectiq.com>
2021-01-25 19:27:03 +0530

    TallyChecker Help added

---
+++ src/components/help/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import "./Spinner.css";
+
+export default function help () {
+  return (
+  <div >
+      <p>This is a test page to create a help page for TallyChekcer help</p>
+  </div>
+  );
+};
+
+//export default help;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,13 +4,14 @@ import { Switch, BrowserRouter, Route, Redirect } from "react-router-dom";
 import Login from "../components/login/login";
 import Container from "../components/Main/Main";
 import PrivateRoute from "./PrivateRoute";
-
+import help from "../components/help";
 const AppRouter = () => {
   return (
     <BrowserRouter basename={"/"}>
       <Switch>
 		<Route exact path="/" component={Login} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/tallycheckerhelp" component={help} />
         <PrivateRoute path="/" component={Container} />
         {/* <Route exact path="/select" component={Layout} /> */}
       </Switch>
